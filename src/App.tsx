f62b19753646748cd0792817479ea199e5a976fc
malzak <malzovich@gmail.com>
2022-04-01 15:40:39 +0100

    Buttons variant, size and color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import { MuiTypography } from "./components/MuiTypography";
+import { MuiButtons } from "./components/MuiButtons";
+// import { MuiTypography } from "./components/MuiTypography";
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
+      {/* <MuiTypography /> */}
+      <MuiButtons />
     </div>
   );
 }
